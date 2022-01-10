7f068350c37733e333354b806f20fcdb697af67b
Angel Rodríguez <18161225@itoaxaca.edu.mx>
2022-01-10 01:36:11 -0600

    Menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Frame, font, ttk, Label, Menu
+from tkinter import Menubutton, Tk, Frame, font, ttk, Label, Menu
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -6,15 +6,22 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 root = Tk()
 root.title("Simulación de COVID-19 México") # Establecemos el titulo
 root.geometry("1000x650") # ancho x alto
+root.configure(background='#CCCCCC')
 
-menubar = Menu(root,selectcolor="black") # Inicializamos el menu
-filemenu = Menu(menubar, tearoff=0) # Inicializamos un submenu para archivo
-filemenu.add_command(label="Abrir") # Añadimos la etiqueta para abrir un archivo y su funcion
-filemenu.add_separator()
-filemenu.add_command(label="Salir",command=root.quit) # Añadimos una opción para salir
-menubar.add_cascade(label="Archivo",menu=filemenu) # Agregamos el submenu de archivo
-root.config(menu=menubar) # Mostramos el menu
+fontStyle = font.Font(family="Calibri",size=12) # Fuente de letra
+barra = Frame(root,background='#1E1E1E',width=1000,height=30) # MenuBar
+barra.place(x=0,y=0) # Establecemos la posicion de la barra
+# Creamos un submenu
+menubar = Menubutton(barra,text="Archivo",background='#1E1E1E',foreground='#CCCCCC',activebackground='#505050',activeforeground='#CCCCCC',font=fontStyle)
+menubar.place(x=1,y=1) # Establecemos la posicion del submenu
+menubar.menu = Menu(menubar, tearoff=0)
+menubar["menu"] = menubar.menu
+# Establecemos las opciones del submenu y sus funciones
+menubar.menu.add_command(label="Cargar",font=fontStyle,activebackground='#094771',activeforeground='#CCCCCC')
+menubar.menu.add_command(label="Salir",font=fontStyle,activebackground='#094771',activeforeground='#CCCCCC')
 
+s = ttk.Style()
+s.configure("TNotebook")
 notebook = ttk.Notebook(root,width=980,height=510)
 tab1 = Frame(notebook)
 tab2 = Frame(notebook)
