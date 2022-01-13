d83c88962debbdd12d9f18bb6cce7c98c2b71960
Angel Rodríguez <18161225@itoaxaca.edu.mx>
2022-01-13 01:43:07 -0600

    Interfaz 1.4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Button, Entry, Menubutton, Radiobutton, StringVar, Tk, Frame, font, ttk, Label, Menu
+from tkinter import Button, Entry, Menubutton, StringVar, Tk, Frame, font, ttk, Label, Menu, filedialog
 from tkinter.constants import CENTER, NO
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -11,6 +11,7 @@ import modelos as md
 class  App:
     listEstado = []
     listNomE = []
+    nameArchivo = 'Datos\\Casos_Diarios.csv'
     
     def __init__(self,master:Tk) -> None:
         self.master = master
@@ -18,8 +19,6 @@ class  App:
         self.master.configure(background='#CCCCCC')
         self.miStyle = font.Font(family="Calibri",size=12) # Fuente de letra
         
-        # Cargamos los datos predeterminados
-        self.Cargar()
         barra = Frame(self.master,background='#1E1E1E',width=2000,height=30) # MenuBar
         barra.place(x=0,y=0) # Establecemos la posicion de la barra
         
@@ -31,7 +30,7 @@ class  App:
         menubar["menu"] = menubar.menu
         
         # Establecemos las opciones del submenu y sus funciones
-        menubar.menu.add_command(label="Cargar",command=self.Cargar,font=self.miStyle,
+        menubar.menu.add_command(label="Cargar",command=self.abrirArchivo,font=self.miStyle,
                                 activebackground='#094771',activeforeground='#CCCCCC')
         menubar.menu.add_command(label="Salir",command=self.master.quit,font=self.miStyle,
                                 activebackground='#094771',activeforeground='#CCCCCC')
@@ -43,7 +42,6 @@ class  App:
         # Creamos un combobox que almacene los estados
         self.cobEstado = ttk.Combobox(state="readonly",font=self.miStyle)
         self.cobEstado.place(x=100,y=80)
-        self.cobEstado['values'] = self.listNomE # Asignamos los datos
         
         #  Creamos un boton para seleccionar los datos
         btnSeleccionar = Button(self.master,text="Seleccionar",font=self.miStyle,command=self.funSeleccionar,
@@ -65,6 +63,9 @@ class  App:
                             activeforeground='#CCCCCC')
         self.btnGop.place(x=200,y=150)
 
+        # Cargamos los datos predeterminados
+        self.Cargar()
+
         # Frame de Trabajo
         self.frameT = Frame(self.master,width=1320,height=500)
         self.frameT.place(x=20,y=184)
@@ -74,8 +75,8 @@ class  App:
         self.txtEstado.set("")
         self.lab_nomEstado = Label(self.frameT,font=("Calibri",25),textvariable=self.txtEstado)
         self.lab_nomEstado.place(x=200,y=10)
-
-        # Frame para el modelo SIR ---------------------------------------------------------------------------------
+# ==================================================================================================================
+        # Frame para el modelo SIR
         self.lab_sTitulo = Label(self.frameT,text="MODELO SIR",font=("Calibri",25))
         # Seleccion de datos sir
         self.str_sn = StringVar()
@@ -113,10 +114,10 @@ class  App:
         self.simularSir = Button(self.frameT,text="SIMULAR",font=self.miStyle,command=self.calcularSIR,
                                 background='#2D2D2D',foreground='#CCCCCC',activebackground='#16825D',
                                 activeforeground='#CCCCCC',)
+        # Grafica SIR
         self.figs, self.axs = plt.subplots(dpi=90,figsize=(5,5))
         self.figs.suptitle('SIR')
         self.can_s = FigureCanvasTkAgg(self.figs,self.frameT)
-        self.can_s.get_tk_widget().place(x=850,y=20)
         # TABLA DE SIR
         self.tab_s = ttk.Treeview(self.frameT,height=21)
         self.tab_s['columns'] = ('t','s','i','r')
@@ -130,22 +131,80 @@ class  App:
         self.tab_s.heading("s",text="S",anchor=CENTER)
         self.tab_s.heading("i",text="I",anchor=CENTER)
         self.tab_s.heading("r",text="R",anchor=CENTER)
-
-        # Freme para el modelo Logistico ----------------------------------------------------------------------------
+# ==================================================================================================================
+        # Freme para el modelo Logistico 
         self.lab_lTitulo = Label(self.frameT,text="MODELO LOGISTICO",font=("Calibri",25))
-        
-
-        # Frame para el modelo Gompertz -----------------------------------------------------------------------------
+        self.lab_l1 = Label(self.frameT,text="Fecha inico:",font=self.miStyle)
+        self.lab_l2 = Label(self.frameT,text="Fecha final:",font=self.miStyle)
+        self.lab_l3 = Label(self.frameT,text="Cantidad de datos:",font=self.miStyle)
+        self.cob_l1 = ttk.Combobox(self.frameT,state="readonly",font=self.miStyle)
+        self.cob_l1.bind('<<ComboboxSelected>>',self.mostarDatosLog)
+        self.cob_l2 = ttk.Combobox(self.frameT,state="readonly",font=self.miStyle)
+        self.cob_l2.bind('<<ComboboxSelected>>',self.mostarDatosLog)
+        self.str_l1 = StringVar()
+        self.str_l1.set("")
+        self.txt_l1 = Entry(self.frameT,font=self.miStyle,textvariable=self.str_l1,state='disabled')
+        self.simularLog = Button(self.frameT,text="SIMULAR",font=self.miStyle,command=self.calcularLog,
+                                background='#2D2D2D',foreground='#CCCCCC',activebackground='#16825D',
+                                activeforeground='#CCCCCC',)
+        # Grafica Logistica
+        self.figl, self.axl = plt.subplots(dpi=90,figsize=(5,5))
+        self.figl.suptitle('Logistico')
+        self.can_l = FigureCanvasTkAgg(self.figl,self.frameT)
+        # TABLA DE logistica
+        self.tab_l = ttk.Treeview(self.frameT,height=21)
+        self.tab_l['columns'] = ('t','P')
+        self.tab_l.column("#0",width=0,stretch=NO)
+        self.tab_l.column("t",anchor=CENTER,width=100)
+        self.tab_l.column("P",anchor=CENTER,width=100)
+        self.tab_l.heading("#0",text="",anchor=CENTER)
+        self.tab_l.heading("t",text="t",anchor=CENTER)
+        self.tab_l.heading("P",text="P",anchor=CENTER)
+# ==================================================================================================================
+        # Frame para el modelo Gompertz
         self.lab_gTitulo = Label(self.frameT,text="MODELO GOMPERTZ",font=("Calibri",25))
-        
+        self.lab_g1 = Label(self.frameT,text="Fecha inico:",font=self.miStyle)
+        self.lab_g2 = Label(self.frameT,text="Fecha final:",font=self.miStyle)
+        self.lab_g3 = Label(self.frameT,text="Cantidad de datos:",font=self.miStyle)
+        self.cob_g1 = ttk.Combobox(self.frameT,state="readonly",font=self.miStyle)
+        self.cob_g1.bind('<<ComboboxSelected>>',self.mostarDatosGop)
+        self.cob_g2 = ttk.Combobox(self.frameT,state="readonly",font=self.miStyle)
+        self.cob_g2.bind('<<ComboboxSelected>>',self.mostarDatosGop)
+        self.str_g1 = StringVar()
+        self.str_g1.set("")
+        self.txt_g1 = Entry(self.frameT,font=self.miStyle,textvariable=self.str_g1,state='disabled')
+        self.simularGop = Button(self.frameT,text="SIMULAR",font=self.miStyle,command=self.calcularGop,
+                                background='#2D2D2D',foreground='#CCCCCC',activebackground='#16825D',
+                                activeforeground='#CCCCCC',)
+        # Grafica Gompertz
+        self.figg, self.axg = plt.subplots(dpi=90,figsize=(5,5))
+        self.figg.suptitle('Gompertz')
+        self.can_g = FigureCanvasTkAgg(self.figg,self.frameT)
+        # TABLA DE Gompertz
+        self.tab_g = ttk.Treeview(self.frameT,height=21)
+        self.tab_g['columns'] = ('t','P','dP')
+        self.tab_g.column("#0",width=0,stretch=NO)
+        self.tab_g.column("t",anchor=CENTER,width=100)
+        self.tab_g.column("P",anchor=CENTER,width=100)
+        self.tab_g.column("dP",anchor=CENTER,width=100)
+        self.tab_g.heading("#0",text="",anchor=CENTER)
+        self.tab_g.heading("t",text="t",anchor=CENTER)
+        self.tab_g.heading("P",text="P",anchor=CENTER)
+        self.tab_g.heading("dP",text="dP",anchor=CENTER)
+# ==================================================================================================================
         # ejercutar metodos de mostrar
         self.fun_btnSir()
-
+# ==================================================================================================================
     # Funcion para Cargar el archivo
+    def abrirArchivo(self):
+        self.nameArchivo = filedialog.askopenfilename()
+        self.Cargar()
+
     def Cargar(self):
-        self.listEstado = lDt.obtecion_datos(archivo='Datos\\Casos_Diarios.csv')
+        self.listEstado = lDt.obtecion_datos(archivo=self.nameArchivo)
         for i in self.listEstado:
             self.listNomE.append(str(i.nombre))
+        self.cobEstado['values'] = self.listNomE # Asignamos los datos
 
     # Funcion del boton seleccionar
     def funSeleccionar(self):
@@ -155,6 +214,10 @@ class  App:
             self.cobF1['values'] = self.Estadoact.fechas
             #print(str(self.cobEstado.current()))
             #poss = self.listEstado.index(self.cobEstado.get())
+            self.cob_l1['values'] = self.Estadoact.fechas
+            self.cob_l2['values'] = self.Estadoact.fechas
+            self.cob_g1['values'] = self.Estadoact.fechas
+            self.cob_g2['values'] = self.Estadoact.fechas
 
     # Calcular los datos de SIR
     def calcularSIR(self):
@@ -177,26 +240,70 @@ class  App:
             for xd in range(0,t):
                 self.tab_s.insert(values=[self.rSir[0][xd],self.rSir[1][xd],self.rSir[2][xd],self.rSir[3][xd]],
                                 parent='',index='end')
-            
-            
 
+    # Calcular los datos Logisticos
+    def calcularLog(self):
+        if self.txt_l1.get() != '':
+            t = int(self.txt_l1.get())
+            pi = self.cob_l1.current()
+            pf = self.cob_l2.current()
+            self.rLog = md.Logistica(t,self.Estadoact.casosAcumulados[pi:pf])
+            self.axl.clear()
+            self.axl.scatter(self.rLog[0],self.rLog[1],label="Logistica")
+            self.axl.legend()
+            self.can_l.draw()
+            self.tab_l.delete(*self.tab_l.get_children())
+            for xd in range(0,t):
+                self.tab_l.insert(values=[self.rLog[0][xd],self.rLog[1][xd]],parent='',index='end')
+
+    # Calcular los datos Gompertz
+    def calcularGop(self):
+        if self.txt_g1.get() != '':
+            t = int(self.txt_g1.get())
+            pi = self.cob_g1.current()
+            pf = self.cob_g2.current()
+            self.rGop = md.gompertz(t,md.cal_constante(self.Estadoact.casosAcumulados[pi:pf]))
+            self.axg.clear()
+            self.axg.scatter(self.rGop[0],self.rGop[1],label="F Gompertz")
+            self.axg.scatter(self.rGop[0],self.rGop[2],label="Derivada")
+            self.axg.legend()
+            self.can_g.draw()
+            self.tab_g.delete(*self.tab_g.get_children())
+            for xd in range(0,t):
+                self.tab_g.insert(values=[self.rGop[0][xd],self.rGop[1][xd],self.rGop[2][xd]],parent='',index='end')
+
+# ==================================================================================================================
     # Funcion para mostrar los datos del sir
     def mostrarDatosSir(self,event):
         if self.cobF1.get() != '':
             self.str_sn.set(str(self.Estadoact.poblacion))
             self.str_ss.set(str(self.Estadoact.poblacion - self.Estadoact.casosDiarios[self.cobF1.current()]))
             self.str_si.set(str(self.Estadoact.casosDiarios[self.cobF1.current()]))
+    
+    # Mostrar la cantidad de datos logistico
+    def mostarDatosLog(self,event):
+        if self.cob_l1.get() != '' and self.cob_l2.get() != '':
+            if self.cob_l2.current()-self.cob_l1.current() > 0:
+                self.str_l1.set(str(self.cob_l2.current()-self.cob_l1.current()))
+            else:
+                self.str_l1.set('')
 
+    # Funcion para mostrar la cantidad de datos de gompertz
+    def mostarDatosGop(self,event):
+        if self.cob_g1.get() != '' and self.cob_g1.get() != '':
+            if self.cob_g2.current()-self.cob_g1.current() > 0:
+                self.str_g1.set(str(self.cob_g2.current()-self.cob_g1.current()))
+            else:
+                self.str_g1.set('')
+
+# ==================================================================================================================
     # Funcion de la visualizacion del modelo sir
     def fun_btnSir(self):
-        # Cambiar color del selector
-        self.btnSir.config(background='#16825D')
-        self.btnLog.config(background='#2D2D2D')
-        self.btnGop.config(background='#2D2D2D')
         # Borrar datos
-        self.lab_lTitulo.place_forget()
-        self.lab_gTitulo.place_forget()
+        self.no_btnLog()
+        self.no_btnGop()
         # Mostrar datos
+        self.btnSir.config(background='#16825D')
         self.lab_sTitulo.place(x=10,y=10)
         self.lab_nomEstado.place(x=200,y=10)
         self.lab_s1.place(x=10,y=100)
@@ -246,22 +353,70 @@ class  App:
         self.can_s.get_tk_widget().place_forget()
         self.tab_s.place_forget()
 
+    # Mostrar la interfaz de logistica
     def fun_btnLog(self):
+        # Borrar datos
         self.no_btnSir()
+        self.no_btnGop()
+        # Mostrar datos
         self.btnLog.config(background='#16825D')
-        self.btnGop.config(background='#2D2D2D')
-        self.lab_gTitulo.place_forget()
-        self.lab_lTitulo.place(x=10,y=10)
         self.lab_nomEstado.place(x=300,y=10)
-        
+        self.lab_lTitulo.place(x=10,y=10)
+        self.lab_l1.place(x=10,y=100)
+        self.lab_l2.place(x=10,y=150)
+        self.lab_l3.place(x=10,y=200)
+        self.cob_l1.place(x=100,y=100)
+        self.cob_l2.place(x=100,y=150)
+        self.txt_l1.place(x=150,y=200)
+        self.simularLog.place(x=300,y=100)
+        self.can_l.get_tk_widget().place(x=850,y=20)
+        self.tab_l.place(x=600,y=20)
 
-    def fun_btnGop(self):
-        self.no_btnSir()
-        self.btnGop.config(background='#16825D')
+    # ocultar los elementos del logistico
+    def no_btnLog(self):
         self.btnLog.config(background='#2D2D2D')
         self.lab_lTitulo.place_forget()
-        self.lab_gTitulo.place(x=10,y=10)
+        self.lab_l1.place_forget()
+        self.lab_l2.place_forget()
+        self.lab_l3.place_forget()
+        self.cob_l1.place_forget()
+        self.cob_l2.place_forget()
+        self.txt_l1.place_forget()
+        self.simularLog.place_forget()
+        self.can_l.get_tk_widget().place_forget()
+        self.tab_l.place_forget()
+
+    def fun_btnGop(self):
+        # Borrar datos
+        self.no_btnSir()
+        self.no_btnLog()
+        # Mostrar datos
+        self.btnGop.config(background='#16825D')
         self.lab_nomEstado.place(x=310,y=10)
+        self.lab_gTitulo.place(x=10,y=10)
+        self.lab_g1.place(x=10,y=100)
+        self.lab_g2.place(x=10,y=150)
+        self.lab_g3.place(x=10,y=200)
+        self.cob_g1.place(x=100,y=100)
+        self.cob_g2.place(x=100,y=150)
+        self.txt_g1.place(x=150,y=200)
+        self.simularGop.place(x=300,y=100)
+        self.can_g.get_tk_widget().place(x=850,y=20)
+        self.tab_g.place(x=500,y=20)
+    
+    def no_btnGop(self):
+        self.btnGop.config(background='#2D2D2D')
+        self.lab_gTitulo.place_forget()
+        self.lab_g1.place_forget()
+        self.lab_g2.place_forget()
+        self.lab_g3.place_forget()
+        self.cob_g1.place_forget()
+        self.cob_g2.place_forget()
+        self.txt_g1.place_forget()
+        self.simularGop.place_forget()
+        self.can_g.get_tk_widget().place_forget()
+        self.tab_g.place_forget()
+# ==================================================================================================================
 
 if __name__  == "__main__":
     #Creamos la ventana
--- modelos.py
+++ modelos.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 # Modelo SIR
 
 def dS(N, S, I, b):
@@ -22,3 +22,41 @@ def SIR(N, S, I, R, t, b=0.199, y=0.0714):
           vt.append(m+2)
       return [vt,vS,vI,vR]
 
+# Modelo Logistico
+def Logistica(t: int, P: list):
+    def p0(P0,t,a=0.1982,b=0.0000006):
+        return (P0*a*np.exp(a*t))/((a-b*P0)+(P0*b*np.exp(a*t)))
+    vt = []
+    vP = []
+    for i in range(0,t):
+        vt.append(i+1)
+        vP.append(p0(P[i],i+1))
+    return [vt,vP]    
+
+# Modelo Gompertz
+def P(a,b,c,t):
+    return a*np.exp(-b*np.exp(-c*t))
+
+def dP(a, b, c, t):
+    return a*b*c*np.exp(-c*t)*np.exp(-b*np.exp(-c*t))
+
+def gompertz(t, const: list):
+    vP = []
+    vdP = []
+    vt = []
+    for m in range(0,t):
+        vP.append(P(const[0],const[1],const[2],m))
+        vdP.append(dP(const[0],const[1],const[2],m))
+        vt.append(m)
+    return [vt,vP,vdP]
+
+def cal_constante(p: list):
+    y = []
+    a = p[len(p)-1]+1
+    for i in p:
+        y.append(np.log(np.log(a/i)))
+    B = ((y[len(y)-1]-y[0])/(len(y)-1))*(-1)+y[0]
+    m = (y[len(y)-1]-y[0])/(len(y)-1)
+    b = np.exp(B)
+    c = -m
+    return [a,b,c]
